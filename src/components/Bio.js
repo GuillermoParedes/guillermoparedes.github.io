b77233bd3938f85de2fbef53337f5ac79fd3ec1c
CHIGY <gdavid.ptorrez@gmail.com>
2018-09-01 20:19:31 -0400

    Agregando links de contactos

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -5,35 +5,119 @@ import 'typeface-montserrat'
 import 'typeface-merriweather'
 
 import profilePic from './me.jpg'
+import githubPic from './github.png'
+import linkedinPic from './linkedin.png'
+import twitterPic from './twitter.png'
+import gmailPic from './gmail.png'
 import { rhythm } from '../utils/typography'
 
 class Bio extends React.Component {
   render() {
     return (
-      <div
-        style={{
-          display: 'flex',
-          marginBottom: rhythm(2.5),
-        }}
-      >
-        <img
-          src={profilePic}
-          alt={`Kyle Mathews`}
+      <div>
+        <div>
+          <img
+            src={profilePic}
+            alt={`Kyle Mathews`}
+            style={{
+              marginRight: rhythm(1 / 2),
+              marginBottom: 0,
+              width: rhythm(2),
+              height: rhythm(2),
+              borderRadius: rhythm(5),
+            }}
+          />
+          <p>
+            Hola!, soy <strong>Guillermo Paredes </strong>
+            Frontend Developer en los últimos años, Backend Developer en algunas
+            ocasiones. He trabajado con equipos de desarrollo y de manera
+            freelancer. Puedes seguirme en twitter como
+            <a href="https://twitter.com/CHIGUI_GD"> @CHIGUI_GD</a>
+          </p>
+        </div>
+        <div
           style={{
-            marginRight: rhythm(1 / 2),
-            marginBottom: 0,
-            width: rhythm(2),
-            height: rhythm(2),
-            borderRadius: rhythm(5),
+            display: 'flex',
+            marginBottom: rhythm(2.5),
           }}
-        />
-        <p>
-          Hola!, soy <strong>Guillermo Paredes </strong>
-          Frontend Developer en los últimos años, Backend Developer en algunas
-          ocasiones. He trabajado con equipos de desarrollo y de manera
-          freelancer. Puedes seguirme en twitter como
-          <a href="https://twitter.com/CHIGUI_GD"> @CHIGUI_GD</a>
-        </p>
+        >
+          <a
+            href="https://github.com/GuillermoParedes"
+            style={{
+              textDecoration: 'none',
+              boxShadow: 'none',
+            }}
+          >
+            <img
+              src={githubPic}
+              alt={`Kyle Mathews`}
+              style={{
+                marginRight: rhythm(1 / 2),
+                marginBottom: 0,
+                width: rhythm(1),
+                height: rhythm(1),
+                borderRadius: rhythm(5),
+              }}
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/guillermo-david-paredes-torrez/"
+            style={{
+              textDecoration: 'none',
+              boxShadow: 'none',
+            }}
+          >
+            <img
+              src={linkedinPic}
+              alt={`https://www.linkedin.com/in/guillermo-david-paredes-torrez/`}
+              style={{
+                marginRight: rhythm(1 / 2),
+                marginBottom: 0,
+                width: rhythm(1),
+                height: rhythm(1),
+                borderRadius: rhythm(5),
+              }}
+            />
+          </a>
+          <a
+            href="https://twitter.com/CHIGUI_GD"
+            style={{
+              textDecoration: 'none',
+              boxShadow: 'none',
+            }}
+          >
+            <img
+              src={twitterPic}
+              alt={`https://twitter.com/CHIGUI_GD`}
+              style={{
+                marginRight: rhythm(1 / 2),
+                marginBottom: 0,
+                width: rhythm(1),
+                height: rhythm(1),
+                borderRadius: rhythm(5),
+              }}
+            />
+          </a>
+          <a
+            href="mailto:gdavid.ptorrez@gmail.com?Subject=Hola"
+            style={{
+              textDecoration: 'none',
+              boxShadow: 'none',
+            }}
+          >
+            <img
+              src={gmailPic}
+              alt={`gdavid.ptorrez@gmail.com`}
+              style={{
+                marginRight: rhythm(1 / 2),
+                marginBottom: 0,
+                width: rhythm(1),
+                height: rhythm(1),
+                borderRadius: rhythm(5),
+              }}
+            />
+          </a>
+        </div>
       </div>
     )
   }
